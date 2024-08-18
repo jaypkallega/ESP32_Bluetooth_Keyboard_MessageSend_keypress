f1f0cb34105e8377254d3c649505512101ebe32a
Jayaprakash Kallega <jayaprakash.kallega@gmail.com>
2024-08-18 16:25:02 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,36 @@
-#include <Arduino.h> 
-#include <BleKeyboard.h>
-#include "BLEHIDDevice.h"
-BleKeyboard bleKeyboard; //starting a keyboard instance
+#include <Arduino.h>
 
+#include <BleKeyboard.h>
 const int buttonPin = 12;
 const int ledPin = 13;
 int buttonState = 0; //Initialise button state as not pressed
+BleKeyboard bleKeyboard("ESP32 JPKB");
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
-
   Serial.begin(115200);
-  Serial.println("Starting Bluetooth");
+  Serial.println("Starting BLE work!");
   bleKeyboard.begin();
-   
 }
 
-// the loop function runs over and over again forever
 void loop() {
-buttonState = digitalRead(buttonPin);
-
-if (buttonState == HIGH) 
+  buttonState = digitalRead(buttonPin);
+  if(bleKeyboard.isConnected() && buttonState == HIGH) 
+  {
+    digitalWrite(ledPin,HIGH);// indicate button press
+    Serial.println("Sending button press message");
+    bleKeyboard.print("Hi bluetooth world");
+    delay(2000);
+    buttonState = 0;
+  }
+else 
 {
-  digitalWrite(ledPin,HIGH);
-   if(bleKeyboard.isConnected()) {
-    Serial.println("Button Pressed. Sending Hi..."); // for serial monitor. To debugging purpose
-    bleKeyboard.print("Welcome to the bluetooth world");
-     }
-}
- else {
+  Serial.println("Button not pressed");
   digitalWrite(ledPin,LOW);
-  Serial.println("Button Off..."); // for serial monitor. To debugging purpose
-  bleKeyboard.print("Hello no world");
 }
-
+  Serial.println("Waiting 1 seconds...");
   delay(1000);
 }
